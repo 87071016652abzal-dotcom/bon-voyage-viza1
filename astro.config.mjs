0e4132117c2a1cacc2e498db1c5d2a0e48023411
87071016652abzal-dotcom <87071016652abzal@gmail.com>
2026-03-02 02:07:57 +0500

    fix: clean astro config syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  // Эта строка разрешает серверный код и отправку форм (убирает ошибку 403)
-  output: 'server', 
+  output: 'server',
   adapter: vercel(),
+});
